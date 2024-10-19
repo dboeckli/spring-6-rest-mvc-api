4c12ec7987641edc9c984a502e96a2bfa7a73ac0
dboeckli <dboeckli@gmail.com>
2024-10-19 10:43:41 +0200

    add everything from base

---
+++ src/main/java/guru/springframework/spring6restmvcapi/entity/Customer.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring6restmvcapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    private UUID id;
+    private String name;
+}
---
+++ src/test/java/guru/springframework/spring6restmvcapi/SfgRestfulBreweryApplicationTest.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6restmvcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SfgRestfulBreweryApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
