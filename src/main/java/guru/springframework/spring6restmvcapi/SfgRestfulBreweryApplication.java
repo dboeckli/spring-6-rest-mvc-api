58757ee31481fc74bb844f017162eec583d67a31
Dominique Boeckli <dboeckli@gmail.com>
2024-10-20 10:14:51 +0200

    Feature/chapter18 spring data rest (#1)
    * add everything from base
    
    * add dependency and requests
    
    * ADD README and some requests for chapter 18

---
+++ src/main/java/guru/springframework/spring6restmvcapi/SfgRestfulBreweryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6restmvcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRestfulBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgRestfulBreweryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/guru/springframework/spring6restmvcapi/entity/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring6restmvcapi.entity;
+
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
